5315c3ef9711b2d2213473de45410d4928d93ba4
CF Grugan <charles_grugan@cable.comcast.com>
2016-09-14 11:04:22 -0400

    bullets at bottom respond to swipe

--- src/xh-automation-ftue.js
+++ src/xh-automation-ftue.js
@@ -7,17 +7,14 @@
       activePage: {
         type: Number,
         reflectToAttribute: true,
-        value: 1,
+        value: 0,
         observer: '_pageChanged'
+      },
+
+      swipeComplete: {
+        type: Boolean,
+        value: true
       }
-    },
-
-    observers: [
-      '_routePageChanged(routeData.page)'
-    ],
-
-    _routePageChanged: function(page) {
-      this.page = page || 'view1';
     },
 
     ready: function() {
@@ -26,10 +23,17 @@
       // this.$.two.style.left = pageWidth + 'px';
     },
 
-    _pageChanged: function(page) {
+    _pageChanged: function() {
+      this.swipeComplete = false;
+      setTimeout(function(){
+        this.swipeComplete = true;
+      }.bind(this), 1);
       // Load page import on demand. Show 404 page if fails
       // var resolvedPageUrl = this.resolveUrl('my-' + page + '.html');
       // this.importHref(resolvedPageUrl, null, this._showPage404, true);
+    },
+    _checkActivePage: function(page) {
+      return (page - 1) === this.activePage;
     }
   });
 }());
